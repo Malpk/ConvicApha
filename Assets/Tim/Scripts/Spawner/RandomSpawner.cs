c99c27c6ab76a2ad57d5ebdf4c84aa297d151d0e
Ger-Net <kawai666mal@gmail.com>
2022-05-17 16:22:11 +0300

    KI

---
+++ Assets/Tim/Scripts/Spawner/RandomSpawner.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomSpawner : MonoBehaviour
+{
+    [SerializeField] private KIFactory kiFactory;
+    [SerializeField] private CircleCollider2D spawnZone;
+    [SerializeField] private float spanwGap;
+    [SerializeField] private AnimationCurve difficultCurve;
+    [SerializeField] private float spawnTimer;
+    [SerializeField] private Tilemap tilemap;
+
+    private Vector3 spawn;
+    private Vector3Int spawnCell;
+    private bool isTimerActive = false;
+    private bool spawned = false;
+
+    private void Update()
+    {
+        if (!isTimerActive)
+            return;
+        spanwGap = difficultCurve.Evaluate(spawnTimer);
+        spawnTimer += Time.deltaTime;
+        
+    }
+    private IEnumerator DebugSpawn()
+    {
+        while (true)
+        {
+            var device = kiFactory.GetRandomKI();
+            spawn = (Random.insideUnitCircle * spawnZone.radius);
+            spawn.x = spawn.x + spawnZone.transform.position.x; 
+            spawn.y = spawn.y + spawnZone.transform.position.y;
+            spawnCell = tilemap.WorldToCell(spawn);
+            if(!(spawnCell.x < 0 || spawnCell.y < 0 || spawnCell.x > 19 || spawnCell.x > 19))
+                device.transform.position = spawnCell;
+            else
+            {
+                CorrectCoordinates();
+                device.transform.position = spawnCell;
+            }
+                
+            yield return new WaitForSeconds(spanwGap);
+        }
+    }
+
+    private void CorrectCoordinates()
+    {
+        if (spawnCell.x < 0)
+            spawnCell.x += 10;
+
+        if (spawnCell.x > 19)
+            spawnCell.x -= 10;  
+
+        if (spawnCell.y < 0)      
+            spawnCell.y += 10;  
+        
+        if (spawnCell.y > 19)
+            spawnCell.y -= 10;
+    }
+    public void Activate()
+    {
+        StartTimer();
+        StartCoroutine(DebugSpawn());
+    }
+
+    public void Deactivate()
+    {
+        StopAllCoroutines();
+    }
+
+    private void StartTimer()
+    {
+        isTimerActive = true;
+    }
+
+    
+}
